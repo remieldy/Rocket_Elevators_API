cf2405b87eef9507365e3f0764f1dbcfee688940
Martin Chantal <martino_1997@hotmail.ca>
2019-03-27 16:03:49 -0400

    git ignore updated

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require_relative 'application'
 #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 # Initialize the Rails application.
 Rails.application.initialize!
+#allo
