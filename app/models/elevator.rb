bfbc7b5045b0ad54a042264668c73609433055d1
Martin Chantal <46904797+supermartino@users.noreply.github.com>
2019-03-29 15:19:57 -0400

    Update elevator.rb

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -14,7 +14,7 @@ class Elevator < ApplicationRecord
  def slack_message()
     notifier = Slack::Notifier.new (ENV["SLACK_API"]) do
         defaults channel: "#elevator_operations",
-                 username: "Elevator_Status"
+                 username: "TeamRemi"
       end
 
       notifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{self.status_was} to #{self.status}"
