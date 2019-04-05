2d0a632312cf801cc13d968db45bd443817e2975
MAMT01 <marc-antoine.m-tanguay.1@ulaval.ca>
2019-04-05 08:42:58 -0400

    changing table name for graphql

--- app/models/building_detail.rb
+++
@@ -1,3 +0,0 @@
-class BuildingDetail < ApplicationRecord
-    belongs_to :building
-end
---
+++ db/migrate/20190312133528_create_buildingdetails.rb
@@ -0,0 +1,9 @@
+class CreateBuildingDetails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :buildingdetails do |t|
+      t.references :building, foreign_key: {on_delete: :cascade, on_update: :cascade}, null: false
+      t.string :information
+      t.string :valeur
+    end
+  end
+end
